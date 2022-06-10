b5748282bf3b8625a72d5de554e14a51d829f36c
LingMi-sama <102707475+LingMi-sama@users.noreply.github.com>
2022-06-10 15:21:03 +0800

    林咕咕

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System.Windows;
+using System.Windows.Input;
+
+namespace LinGuGu2
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Border_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if(e.ChangedButton == MouseButton.Left)
+            {
+                this.DragMove();
+            }
+
+        }
+
+        bool IsMaximized = false;
+        private void Boreder_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            if(e.ClickCount ==2)
+            {
+                if(IsMaximized)
+                {
+                    this.WindowState = WindowState.Normal;
+                    this.Width = 1250;
+                    this.Height = 830;
+
+                    IsMaximized = false;
+                }
+                else
+                {
+                    this.WindowState = WindowState.Maximized;
+                    IsMaximized = true;
+                }
+            }
+        }
+    }
+}
---
+++ UserControls/MyMessageChat.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace LinGuGu2.UserControls
+{
+    public partial class MyMessageChat : UserControl
+    {
+        public MyMessageChat()
+        {
+            InitializeComponent();
+        }
+
+        public string Message
+        {
+            get { return (string)GetValue(MessageProperty); }
+            set { SetValue(MessageProperty, value); }
+        }
+
+        public static readonly DependencyProperty MessageProperty = DependencyProperty.Register("Message", typeof(string), typeof(MyMessageChat));
+
+    }
+}
